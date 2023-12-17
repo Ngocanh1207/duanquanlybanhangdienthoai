a445efdcfbcd484488a457f99cae04a7750b1b0b
Ngocanh1207 <154047322+Ngocanh1207@users.noreply.github.com>
2023-12-17 19:23:55 +0700

    Update HienThiKhuyenMai.java

--- src/bandienthoai/HienThiKhuyenMai.java
+++ src/bandienthoai/HienThiKhuyenMai.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package giaodienchuan.model.FrontEnd.FormHienThi;
+package bandienthoai.model.FrontEnd.FormHienThi;
 
-import giaodienchuan.model.BackEnd.PriceFormatter;
-import giaodienchuan.model.BackEnd.QuanLyKhuyenMai.KhuyenMai;
-import giaodienchuan.model.BackEnd.QuanLyKhuyenMai.QuanLyKhuyenMaiBUS;
-import giaodienchuan.model.FrontEnd.GiaoDienChuan.MyTable;
+import bandienthoai.model.BackEnd.PriceFormatter;
+import bandienthoai.model.BackEnd.QuanLyKhuyenMai.KhuyenMai;
+import bandienthoai.model.BackEnd.QuanLyKhuyenMai.QuanLyKhuyenMaiBUS;
+import bandienthoai.model.FrontEnd.bandienthoai.MyTable;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
